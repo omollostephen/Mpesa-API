55fcad788d945d9cd674ed08a9d127976100171a
StepTech <omollostephen62@gmail.com>
2024-08-09 16:31:48 +0300

    Sending query file

--- callback.php
+++ callback.php
@@ -8,21 +8,21 @@ $log = fopen($logFile, "a");
 fwrite($log, $stkCallbackResponse);
 fclose();
 
-// $data = json_decode($stkCallbackResponse);
+$data = json_decode($stkCallbackResponse);
 
-// $MerchantRequestID = $data->Body->stkCallback->MerchantRequestID;
-// $CheckoutRequestID = $data->Body->stkCallback->CheckoutRequestID;
-// $ResultCode = $data->Body->stkCallback->ResultCode;
-// $ResultDesc = $data->Body->stkCallback->ResultDesc;
-// $Amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->value;
-// $TransactionId = $data->Body->stkCallback->CallbackMetadata->Item[1]->value;
-// $phone = $data->Body->stkCallback->CallbackMetadata->Item[4]->value;
+$MerchantRequestID = $data->Body->stkCallback->MerchantRequestID;
+$CheckoutRequestID = $data->Body->stkCallback->CheckoutRequestID;
+$ResultCode = $data->Body->stkCallback->ResultCode;
+$ResultDesc = $data->Body->stkCallback->ResultDesc;
+$Amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->value;
+$TransactionId = $data->Body->stkCallback->CallbackMetadata->Item[1]->value;
+$phone = $data->Body->stkCallback->CallbackMetadata->Item[4]->value;
 //CHECK IF TRANSACTION IS SUCCESSFUL
-// if($ResultCode == 0){
-    //STORE THE TRANSACTION DETAILS IN THE DATABASE
-    // mysql_query($db, "INSERT INTO payments (MerchantRequestID, CheckoutRequestID, ResultCode, MpesaReceiptNumber Amount, MpesaPhoneNo,)
-    //  VALUES('$MerchantRequestID','$CheckoutRequestID','$ResultCode','$TransactionId','$Amount','$phone')");
-//     mysql_query($db,"INSERT INTO payments (MerchantRequestID, CheckoutRequestID, ResultCode, MpesaReceiptNumber Amount, MpesaPhoneNo,) VALUES
-//     ('$MerchantRequestID','$CheckoutRequestID','$ResultCode','$TransactionId','$Amount','$phone')");
-// }
+if($ResultCode == 0){
+   // STORE THE TRANSACTION DETAILS IN THE DATABASE
+    mysql_query($db, "INSERT INTO payments (MerchantRequestID, CheckoutRequestID, ResultCode, MpesaReceiptNumber Amount, MpesaPhoneNo,)
+     VALUES('$MerchantRequestID','$CheckoutRequestID','$ResultCode','$TransactionId','$Amount','$phone')");
+    mysql_query($db,"INSERT INTO payments (MerchantRequestID, CheckoutRequestID, ResultCode, MpesaReceiptNumber Amount, MpesaPhoneNo,) VALUES
+    ('$MerchantRequestID','$CheckoutRequestID','$ResultCode','$TransactionId','$Amount','$phone')");
+}
 ?>
